900c30fbdc6d4c4a5d5403cbbb866ef7893190d8
kamsiOrazulike <kamsi_orazulike@icloud.com>
2024-01-30 16:42:42 +0000

    footer change

--- gc37/app/(main)/components/footer.tsx
+++ gc37/app/(main)/components/footer.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 const Footer = () => {
   const [showFooter, setShowFooter] = useState(false);
@@ -37,29 +36,27 @@ const Footer = () => {
 
   return (
     <>
-      {showFooter && (
-        <footer className="fixed bg-transparent bottom-0 right-100 w-full md:flex-row md:flex-nowrap md:justify-start flex items-start p-4 z-40">
-          <div>
-            <div
-              className={`text-white transition-all duration-150 font-light text-xs mx-auto px-4 py-2 `}
+      <footer className="fixed bg-transparent bottom-0 right-100 w-full md:flex-row md:flex-nowrap md:justify-start flex items-start p-4 z-40">
+        <div>
+          <div
+            className={`text-white transition-all duration-150 font-light text-xs mx-auto px-4 py-2 `}
+          >
+            <a
+              href="https://www.rca.ac.uk/study/schools/school-design/grand-challenge/"
+              target="_blank"
+              className="text-xl"
             >
-              <a
-                href="https://www.rca.ac.uk/study/schools/school-design/grand-challenge/"
-                target="_blank"
-                className="text-xl"
-              >
-                <img
-                  src={
-                    "https://made.partners/wp-content/uploads/2017/04/rca-logo-768x332.png"
-                  }
-                  className="w-[8rem] mt-4"
-                  alt="Education 2"
-                />
-              </a>
-            </div>
+              <img
+                src={
+                  "https://made.partners/wp-content/uploads/2017/04/rca-logo-768x332.png"
+                }
+                className="w-[8rem] mt-4"
+                alt="Education 2"
+              />
+            </a>
           </div>
-        </footer>
-      )}
+        </div>
+      </footer>
     </>
   );
 };
