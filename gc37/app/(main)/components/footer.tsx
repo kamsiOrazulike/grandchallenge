524de6b70e709c5acf59978bf1ce4ecdae02d6d3
kamsiOrazulike <kamsi_orazulike@icloud.com>
2024-02-01 12:53:44 +0000

    updates

--- gc37/app/(main)/components/footer.tsx
+++ gc37/app/(main)/components/footer.tsx
@@ -36,7 +36,7 @@ const Footer = () => {
 
   return (
     <>
-      <footer className="fixed bg-transparent bottom-0 right-100 w-full md:flex-row md:flex-nowrap md:justify-start flex items-start p-4 z-40">
+      <footer className="relative bg-[#292E36] bottom-0 right-100 w-full md:flex-row md:flex-nowrap md:justify-start flex items-center p-4 z-40">
         <div>
           <div
             className={`text-white transition-all duration-150 font-light text-xs mx-auto px-4 py-2 `}
@@ -48,13 +48,51 @@ const Footer = () => {
             >
               <img
                 src={
-                  "https://made.partners/wp-content/uploads/2017/04/rca-logo-768x332.png"
+                  "https://autonomy.work/wp-content/uploads/2020/09/pngkey.com-rca-logo-png-5312124.png"
                 }
                 className="w-[8rem] mt-4"
-                alt="Education 2"
+                alt="rca logo"
               />
             </a>
           </div>
+        </div>
+        <div className="ml-auto flex items-end">
+          <ul className="flex flex-wrap items-end justify-end text-sm md:text-md space-x-4 uppercase text-white/40">
+            <li className="hover:text-white hover:cursor-pointer transition-all duration-150">
+              <a
+                href="https://www.instagram.com/k0.design?igsh=MTJrMDJid2pubmFkbw%3D%3D&utm_source=qr"
+                target="_blank"
+              >
+                Kamsi Orazulike
+              </a>
+            </li>
+            <li className="hover:text-white hover:cursor-pointer transition-all duration-150">
+              <a
+                href="https://www.instagram.com/lilyrui0422?igsh=NTc4MTIwNjQ2YQ=="
+                target="_blank"
+              >
+                Jiarui Wang
+              </a>
+            </li>
+            <li className="hover:text-white hover:cursor-pointer transition-all duration-150">
+              <a
+                href="https://www.instagram.com/shen_xu_nn?igsh=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr"
+                target="_blank"
+              >
+                Shen Xu
+              </a>
+            </li>
+            <li className="hover:text-white hover:cursor-pointer transition-all duration-150">
+              <a href="#" target="_blank">
+                Jieying Wen
+              </a>
+            </li>
+            <li className="hover:text-white hover:cursor-pointer transition-all duration-150">
+              <a href="#" target="_blank">
+                Flora Lindsay
+              </a>
+            </li>
+          </ul>
         </div>
       </footer>
     </>
